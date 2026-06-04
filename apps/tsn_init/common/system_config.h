a7e7d48b8c62950639342d8d19f3181859134305
Aziz Sellami <aziz.sellami@nxp.com>
2026-06-04 11:00:46 +0200

    apps: tsn_init: system_config: use storage for net config
    Signed-off-by: Aziz Sellami <aziz.sellami@nxp.com>

--- apps/tsn_init/common/system_config.h
+++ apps/tsn_init/common/system_config.h
@@ -16,11 +16,14 @@
 
 struct net_config {
     uint8_t hw_addr[6];
+    uint8_t ip_addr[4];
+    uint8_t net_mask[4];
+    uint8_t gw_addr[4];
 };
 
 extern struct net_config system_net_cfg[CONFIG_APP_LOGICAL_PORTS];
 
+void __system_config_get_net(const char *prefix, unsigned int port_id, struct net_config *net);
 struct net_config *system_config_get_net(unsigned int port_id);
-
 
 #endif /* _SYSTEM_CONFIG_H_ */
